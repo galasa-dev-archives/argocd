52f8253651550c406eeec48f0061f8f0872216c6
Michael Baylis <Michael.Baylis@uk.ibm.com>
2022-02-09 14:55:21 +0000

    update scripts

---
+++ releasePipeline/01-create-namespace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+kubectl create namespace galasa-branch-release
+
+kubectl get secret gpgkey --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
+kubectl get secret gpggradle --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
+kubectl get secret mavengpg --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
+kubectl get secret harbor-user-pass --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
+kubectl get secret galasadev-cert --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
+kubectl get secret github-creds --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
+kubectl get secret maven-creds --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
+
+echo "Complete"
---
+++ releasePipeline/31-deploy_docker_galasa.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+FROM=release
+
+TO=x.xx.x
+
+ibmcloud cr login
+
+
+
+
+docker pull harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-p2:$FROM
+docker pull harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-javadocs:$FROM
+docker pull harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-boot-embedded-amd64:$FROM
+
+
+
+docker tag harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-p2:$FROM                      \
+           icr.io/galasadev/galasa-p2-amd64:$TO
+
+docker tag harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-javadocs:$FROM                  \
+           icr.io/galasadev/galasa-javadoc-amd64:$TO
+
+docker tag harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-boot-embedded-amd64:$FROM       \
+           icr.io/galasadev/galasa-boot-embedded-amd64:$TO
+
+
+
+docker tag harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-p2:$FROM                       \
+           icr.io/galasadev/galasa-p2-amd64:latest
+
+docker tag harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-javadocs:$FROM                \
+           icr.io/galasadev/galasa-javadoc-amd64:latest
+
+docker tag harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-boot-embedded-amd64:$FROM      \
+           icr.io/galasadev/galasa-boot-embedded-amd64:latest
+
+
+
+docker push icr.io/galasadev/galasa-p2-amd64:$TO
+docker push icr.io/galasadev/galasa-javadoc-amd64:$TO
+docker push icr.io/galasadev/galasa-boot-embedded-amd64:$TO
+
+
+
+docker push icr.io/galasadev/galasa-p2-amd64:latest
+docker push icr.io/galasadev/galasa-javadoc-amd64:latest
+docker push icr.io/galasadev/galasa-boot-embedded-amd64:latest
