d455aa284e2f3d886ba07b73fbda0abd6cdeed6f
Michael Baylis <Michael.Baylis@uk.ibm.com>
2022-02-09 15:36:49 +0000

    needs schema

--- releasePipeline/01-create-namespace.sh
+++ releasePipeline/01-create-namespace.sh
@@ -11,5 +11,6 @@ kubectl get secret harbor-user-pass --namespace=galasa-tekton -o json | jq 'del(
 kubectl get secret galasadev-cert --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
 kubectl get secret github-creds --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
 kubectl get secret maven-creds --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
+kubectl get secret harbor-creds --namespace=galasa-tekton -o json | jq 'del(.metadata.namespace,.metadata.resourceVersion,.metadata.uid, .metadata.creationTimestamp, .metadata.selfLink)' | kubectl apply --namespace=galasa-branch-release -f -
 
 echo "Complete"
