a12e05bb3b18ef3ddf2bcf460410d90bf5a8d44d
Michael Baylis <Michael.Baylis@uk.ibm.com>
2022-02-13 12:40:17 +0000

    Add docker operator

---
+++ releasePipeline/36-deploy-docker-docker-operator.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+FROM=release
+
+TO=x.xx.x
+
+ibmcloud cr login
+
+
+
+
+docker pull harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-docker-operator-amd64:$FROM
+
+
+
+docker tag harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-docker-operator-amd64:$FROM                      \
+           icr.io/galasadev/galasa-cli-amd64:$TO
+
+
+
+docker tag harbor-cicsk8s.hursley.ibm.com/galasadev/galasa-docker-operator-amd64:$FROM                       \
+           icr.io/galasadev/galasa-cli-amd64:latest
+
+
+
+
+docker push icr.io/galasadev/galasa-docker-operator-amd64:$TO
+
+
+
+docker push icr.io/galasadev/galasa-docker-operator-amd64:latest
